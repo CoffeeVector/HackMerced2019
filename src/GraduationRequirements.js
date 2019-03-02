354f770cab3174aef25e12fd074d790b5d42deae
coffeevector <coffeevector@gmail.com>
2019-03-02 01:44:29 -0800

    Moved general education out of render method

--- src/GraduationRequirements.js
+++ src/GraduationRequirements.js
@@ -13,6 +13,20 @@ const majors = [
 	{value: 'math', label: 'Applied Mathematics'}
 ]
 
+const generalEducation = (
+	<div>
+		<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center", margin: "1vw"}}>
+			General Education Requirements
+		</div>
+		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
+			Lower Division General Education
+		</div>
+		<Requirement name="Spark Seminar" courses={[["SPARK", "001"]]}/>
+		<Requirement name="Written Communication" courses={[["WRI", "010"]]}/>
+		<Requirement name="Quantitative Reasoning" courses={[["ECON", "010"], ["MATH", "011"], ["MATH", "021"], ["PHIL", "005"] , ["POLI", "010"], ["PSY", "010"], ["SOC", "010"]]}/>
+	</div>
+);
+
 const majorToRequirement = {
 }
 
@@ -31,19 +45,6 @@ class GraduationRequirements extends Component {
 
 	render() {
 		const selectedMajor = this.state.major
-		const generalEducation = (
-			<div>
-				<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center", margin: "1vw"}}>
-					General Education Requirements
-				</div>
-				<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
-					Lower Division General Education
-				</div>
-				<Requirement name="Spark Seminar" courses={[["SPARK", "001"]]}/>
-				<Requirement name="Written Communication" courses={[["WRI", "010"]]}/>
-				<Requirement name="Quantitative Reasoning" courses={[["ECON", "010"], ["MATH", "011"], ["MATH", "021"], ["PHIL", "005"] , ["POLI", "010"], ["PSY", "010"], ["SOC", "010"]]}/>
-			</div>
-		);
 		return (
 			<div style={stylization}>
 				<div style={{fontSize: "calc(10px + 2vmin)", textAlign: "center"}}>
