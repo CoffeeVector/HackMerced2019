1826d43eb66708e4bf8859f128af130ecd3a0c10
coffeevector <coffeevector@gmail.com>
2019-03-01 22:12:37 -0800

    Removed padding to remove horizontal scrollbar

--- src/GraduationRequirements.js
+++ src/GraduationRequirements.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 const stylization = {
 	backgroundColor: "#C5C8C6",
-	marginLeft: "1vw",
-	marginRight: "1vw"
 }
 class GraduationRequirements extends Component {
 	render() {
