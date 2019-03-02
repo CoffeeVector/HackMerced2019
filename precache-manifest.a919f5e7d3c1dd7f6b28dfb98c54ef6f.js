e801adec22c0170d8e68cd71ba0ee22f62ad5063
coffeevector <coffeevector@gmail.com>
2019-03-01 19:41:06 -0800

    Updates

---
+++ precache-manifest.a919f5e7d3c1dd7f6b28dfb98c54ef6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fa7829d488f134223c0",
+    "url": "/HackMerced2019/static/css/main.d35fab62.chunk.css"
+  },
+  {
+    "revision": "3fa7829d488f134223c0",
+    "url": "/HackMerced2019/static/js/main.3fa7829d.chunk.js"
+  },
+  {
+    "revision": "6c75ea543ec84c7ca707",
+    "url": "/HackMerced2019/static/js/runtime~main.6c75ea54.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/HackMerced2019/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "98e8e804929a1f42b8ad5c52dbbf7424",
+    "url": "/HackMerced2019/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/HackMerced2019/precache-manifest.a919f5e7d3c1dd7f6b28dfb98c54ef6f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/HackMerced2019/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
