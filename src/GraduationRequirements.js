a014b19fb0bffba20c08dc4e5c17a2c7034920a3
coffeevector <coffeevector@gmail.com>
2019-03-02 01:07:45 -0800

    Added Requirement class and some basic general reuqirements for testing

--- src/GraduationRequirements.js
+++ src/GraduationRequirements.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
+import Requirement from './Requirement.js'
 
 const stylization = {
 	backgroundColor: "#C5C8C6",
 	overflow: "visible"
 }
-
 const majors = [
 	{value: 'cse', label: 'Computer Science & Engineering'},
 	{value: 'me', label: 'Mechanical Engineering'},
@@ -32,6 +32,19 @@ class GraduationRequirements extends Component {
 
 	render() {
 		const selectedMajor = this.state.major
+		const generalEducation = (
+			<div>
+				<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center"}}>
+					General Education Requirements
+				</div>
+				<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left"}}>
+					Lower Division General Education
+				</div>
+				<Requirement name="Spark Seminar" courses={[["SPARK", "001"]]}/>
+				<Requirement name="Written Communication" courses={[["WRI", "010"]]}/>
+				<Requirement name="Quantitative Reasoning" courses={[["ECON", "010"], ["MATH", "011"], ["MATH", "021"], ["PHIL", "005"] , ["POLI", "010"], ["PSY", "010"], ["SOC", "010"]]}/>
+			</div>
+		);
 		return (
 			<div style={stylization}>
 				<div style={{fontSize: "calc(10px + 2vmin)", textAlign: "center"}}>
@@ -42,6 +55,9 @@ class GraduationRequirements extends Component {
 					onChange={this.handleChange}
 					options={majors}
 				/>
+				{(this.state.major == null) ? "":
+						generalEducation
+				}
 			</div>
 		);
 	}
