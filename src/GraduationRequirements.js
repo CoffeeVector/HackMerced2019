cf0cd45b1818c89a2ba3b56613e6303b2770e625
coffeevector <coffeevector@gmail.com>
2019-03-02 02:23:39 -0800

    Added more requirements

--- src/GraduationRequirements.js
+++ src/GraduationRequirements.js
@@ -21,13 +21,61 @@ const generalEducation = (
 		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
 			Lower Division General Education
 		</div>
-		<Requirement name="Spark Seminar" courses={[["SPARK", "001"]]}/>
-		<Requirement name="Written Communication" courses={[["WRI", "010"]]}/>
-		<Requirement name="Quantitative Reasoning" courses={[["ECON", "010"], ["MATH", "011"], ["MATH", "021"], ["PHIL", "005"] , ["POLI", "010"], ["PSY", "010"], ["SOC", "010"]]}/>
+		<Requirement name="Spark Seminar" logic="all" courses={[["SPARK", "001"]]}/>
+		<Requirement name="Written Communication" logic="all" courses={[["WRI", "010"]]}/>
+		<Requirement name="Quantitative Reasoning" logic="any" courses={[["ECON", "010"], ["MATH", "011"], ["MATH", "021"], ["PHIL", "005"] , ["POLI", "010"], ["PSY", "010"], ["SOC", "010"]]}/>
+		<Requirement name="Language" logic="any" courses={[["BIOE", "021"], ["CHN", "002"], ["CSE", "021"], ["FRE", "002"], ["JPN", "002"], ["ME", "021"], ["SPAN", "002"]]}/>
 	</div>
 );
 
 const majorToRequirement = {
+	'cse': <div>
+		<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center", margin: "1vw"}}>
+			Major Requirements
+		</div>
+		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
+			Required Major Preparation [32 Units]
+		</div>
+		<Requirement name="Mathematics Requirement [20 units]" logic="all" courses={[["MATH", "021"], ["MATH", "022"], ["MATH", "023"], ["MATH", "024"], ["MATH", "032"]]}/>
+		<Requirement name="Physics Requirement [8 units]" logic="all" courses={[["PHYS", "008"], ["PHYS", "009"]]}/>
+		<Requirement name="Biological or Earth Systems Science Requirement [4 units]" logic="any" courses={[["PHYS", "008"], ["PHYS", "009"]]}/>
+		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
+			Computer Science and Engineering Core [28 units]
+		</div>
+		<Requirement name="Lower Division Requirement [20 units]" logic="all" courses={[["CSE", "015"], ["CSE", "020"], ["CSE", "021"], ["CSE", "030"], ["CSE", "031"], ["ENGR", "065"]]}/>
+		<Requirement name="Upper Division Requirement [8 units]" logic="all" courses={[["CSE", "100"], ["CSE", "120"]]}/>
+		<Requirement name="Technical Electives Requirement [30 units]" logic="30 units" courses={[["CSE", "1**"]]}/>
+	</div>,
+	'me': <div>
+		<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center", margin: "1vw"}}>
+			Major Requirements
+		</div>
+		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
+			Required Major Preparation [32 Units]
+		</div>
+		<Requirement name="Mathematics Requirement [20 units]" logic="all" courses={[["MATH", "021"], ["MATH", "022"], ["MATH", "023"], ["MATH", "024"], ["MATH", "032"]]}/>
+		<Requirement name="Physics Requirement [8 units]" logic="all" courses={[["PHYS", "008"], ["PHYS", "009"]]}/>
+		<Requirement name="Biological or Earth Systems Science Requirement [4 units]" logic="any" courses={[["PHYS", "008"], ["PHYS", "009"]]}/>
+	</div>,
+	'bio': <div>
+		<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center", margin: "1vw"}}>
+			Major Requirements
+		</div>
+		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
+			Requirements for Biological Sciences Major
+		</div>
+		<Requirement name="Biological Sciences Requirement [14 units]" logic="all" courses={[["BIO", "001"], ["BIO", "001L"], ["BIO", "002"], ["BIO", "002L"], ["BIO", "110"]]}/>
+	</div>,
+	'phys':<div>
+		<div style={{fontSize: "calc(7px + 2vmin)", textAlign: "center", margin: "1vw"}}>
+			Major Requirements
+		</div>
+		<div style={{fontSize: "calc(5px + 2vmin)", textAlign: "left", margin: "1vw"}}>
+			Requirements for Physics Major
+		</div>
+		<Requirement name="Biological Sciences Requirement [14 units]" logic="all" courses={[["BIO", "001"], ["BIO", "001L"], ["BIO", "002"], ["BIO", "002L"], ["BIO", "110"]]}/>
+	</div>,
+
 }
 
 class GraduationRequirements extends Component {
@@ -58,7 +106,11 @@ class GraduationRequirements extends Component {
 					/>
 				</div>
 				{(this.state.major == null) ? "":
-						generalEducation
+						<div>
+							{generalEducation}
+							{majorToRequirement[this.state.major.value]}
+						</div>
+
 				}
 			</div>
 		);
