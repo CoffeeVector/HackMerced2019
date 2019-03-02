0198456831e1c361de5722cbead95c47e99d6244
coffeevector <coffeevector@gmail.com>
2019-03-02 01:16:11 -0800

    Changed plan background color

--- src/Plan.js
+++ src/Plan.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 const stylization = {
-	backgroundColor: "#FF0000",
+	backgroundColor: "#C5EDEA"
 }
 class Plan extends Component {
 	render() {
@@ -10,19 +10,19 @@ class Plan extends Component {
 				Plan
 			</div>
 		);
-		// PSEUDO: Once the major is selected, list semesters 
-		// for fall and spring and let them input the classes that 
-		// they have taken and plan to take. 
-			
-			// PSEUDO: While they're inputing their courses, we 
-			// need to have a drop down menu of suggested classes
-			// that they could be typing. (If we got the time)
-			
-	// PSEUDO: Hard part, take the courses that they've taken
-	// and the ones they plan to take and determine if these 
-	// will fulfill their course requirements. 
-		
-		
+		// PSEUDO: Once the major is selected, list semesters
+		// for fall and spring and let them input the classes that
+		// they have taken and plan to take.
+
+		// PSEUDO: While they're inputing their courses, we
+		// need to have a drop down menu of suggested classes
+		// that they could be typing. (If we got the time)
+
+		// PSEUDO: Hard part, take the courses that they've taken
+		// and the ones they plan to take and determine if these
+		// will fulfill their course requirements.
+
+
 	}
 }
 
