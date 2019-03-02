76d22b36a31881ec39d124bf953fe342191e6575
Alex <ajw006@gmail.com>
2019-03-02 00:56:29 -0800

    Another comment

--- src/Plan.js
+++ src/Plan.js
@@ -10,6 +10,19 @@ class Plan extends Component {
 				Plan
 			</div>
 		);
+		// PSEUDO: Once the major is selected, list semesters 
+		// for fall and spring and let them input the classes that 
+		// they have taken and plan to take. 
+			
+			// PSEUDO: While they're inputing their courses, we 
+			// need to have a drop down menu of suggested classes
+			// that they could be typing. (If we got the time)
+			
+	// PSEUDO: Hard part, take the courses that they've taken
+	// and the ones they plan to take and determine if these 
+	// will fulfill their course requirements. 
+		
+		
 	}
 }
 
